815eb91428b7dbdf355e3528e4a50b6474520fc5
shokhista <shokhistakhashimova@gmail.com>
2024-08-07 08:37:09 +0300

    homewor_4_shokhista_done

---
+++ homework/shokhista_khashimova/homework_4/task_2.py
@@ -0,0 +1,16 @@
+my_dict = {'tuple': (1, "q", "A", "z", "word"), 'list': ["first", "second", "third", "cat", "dog"], 
+           'dict': {'car1': 'volvo', 'car2': 'bmw'}, 'set': {"white", "black", "yellow", 2, 6.8}}
+
+
+print(my_dict['tuple'][-1])
+
+my_dict['list'].append('new')
+my_dict['list'].pop(1)
+
+my_dict['dict']['i am a tuple'] = False
+my_dict['dict'].pop('car1')
+
+my_dict['set'].add("Yes")
+my_dict['set'].pop()
+
+print(my_dict)
